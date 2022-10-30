bdfbd662e180de8a5c2eda8c7f793a23365ab30e
Muhamed Agic <38727392+Muhamedhro@users.noreply.github.com>
2022-10-30 15:48:26 +0100

    verder met mui puzzel

--- learning_rust/src/puzzles_and_challenges.rs
+++ learning_rust/src/puzzles_and_challenges.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 use std::vec::Vec;
+use core::str::CharIndices;
 
 // mu puzzle
 // Rules and possibilities:
@@ -10,6 +11,7 @@ use std::vec::Vec;
 
 // CHALLENGE: start with "mi"
 // Make transformations such that you end with "mu"
+
 #[derive(Debug)]
 pub enum Rules
 {
@@ -19,21 +21,48 @@ pub enum Rules
     delete_2_successive_us = 3
 }
 
-pub fn has_three_successive_is() -> ()
+// returns where x successive chars are in a string slice
+// for example: get succesive uu's in "miiuiuuiimiuu" would return  ('u',[[5, 6], [11, 12]])
+pub fn has_this_amount_successive_chars(chars_in_element: &mut CharIndices, char_to_look_for: &char, x_in_a_row: u32) -> bool
 {
-    todo!();
+    let mut count = 0;
+    for i in 0..x_in_a_row
+    {
+        if chars_in_element.next().unwrap().1 == *char_to_look_for
+        {
+           count += 1;
+        }
+    }
+    // Weer x aantal plaatsen terug gaan
+    chars_in_element.rev();
+    for i in 0..x_in_a_row
+    {
+        chars_in_element.next();
+    }
+    // Weer vooruit willen gaan
+    chars_in_element.rev();
+    // Net zo veel character op een rij gevonden als dat gevonden moest worden
+    if count == x_in_a_row
+    {
+        return true;
+    }
+    return false;
 }
 
-// Return occurances of a character of an element with its index(es)
-pub fn return_occurances_and_indices_of(char_to_find: char, element: &str) -> Option<Vec<usize>>
+// Return occurances of a character, with its index(es), which occures x times in a row, of an element (string slice)
+pub fn return_occurances_and_indices_of(element: &str, char_to_find: char, x_in_a_row: u32) -> Option<Vec<usize>>
 {
     let mut char_indices = Vec::new();
     let mut chars = element.char_indices();
+    let mut chars_copy = element.char_indices(); // Bewust een kopie
+
     chars.into_iter()
          .for_each(|index_char_tuple| if index_char_tuple.1 == char_to_find 
                                                      { 
-                                                         
-                                                        char_indices.push(index_char_tuple.0.clone())
+                                                         if has_this_amount_successive_chars(&mut chars_copy, &char_to_find, x_in_a_row)
+                                                         {
+                                                             char_indices.push(index_char_tuple.0.clone())    
+                                                         }
                                                      }
                   );
 
@@ -54,11 +83,26 @@ pub fn check_rules_to_apply(element: &str) -> [bool; 4]
     }
 
     // Three 'I's in succession can be changed to a 'U' ('MUIII' can be changed to 'MUU')
+    let iii_indices: Option<Vec<usize>> = return_occurances_and_indices_of(element, 'i', 3);
+    if iii_indices != None
+    {
+        //rules_to_apply[Rules::change_three_is_to_u] = true;
+
+    }
 
     // The string 'Mx' (where x is any sequence of letters) can be changed to 'Mxx' ('MUIU' can be changed to 'MUIUUIU')
+    let m_indices: Option<Vec<usize>> = return_occurances_and_indices_of(element, 'm', 1);
+    if m_indices != None
+    {
+        //rules_to_apply[Rules::duplicate_everything_after_m] = true;
+    }
 
     // Two 'U's in succession can be deleted ('MIUU' can be changed to 'MI')
-
+    let uu_indices: Option<Vec<usize>> = return_occurances_and_indices_of(element, 'u', 2);
+    if uu_indices != None
+    {
+        //rules_to_apply[Rules::delete_2_successive_us] = true;
+    }
 
 
     return rules_to_apply;
